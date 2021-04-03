84119daa4808f8235964120444e7fac166edad5b
Lysindr <vetercwg@gmail.com>
2021-04-03 23:39:32 +0300

    Add Nuxt JS base APP.

---
+++ components/Navigation.vue
@@ -0,0 +1,19 @@
+<template>
+	<nav class="navigation">
+		<NuxtLink to="/">Home</NuxtLink>
+		<NuxtLink to="/about-us">About us</NuxtLink>
+		<NuxtLink to="/contact-us">Contact us</NuxtLink>
+	</nav>
+</template>
+
+<style>
+	.navigation {
+		margin: 15px 0;
+		padding: 15px 10px;
+	}
+
+	.navigation a {
+		display: inline-block;
+		margin: 0 10px;
+	}
+</style>
---
+++ pages/contact-us.vue
@@ -0,0 +1,17 @@
+<template>
+	<div class="container">
+		<div>
+			<Logo />
+
+			<Navigation />
+
+			<h1 class="title">
+				Contact Us
+			</h1>
+		</div>		
+	</div>
+</template>
+
+<script>
+	export default {}
+</script>
